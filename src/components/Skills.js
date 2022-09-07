02cce09b1ef3b736c46cbe2d20b8e26d953bb806
Maximiliano Minutillo <maximilminu@gmail.com>
2022-09-07 10:10:08 -0300

    estilos Skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -20,68 +20,66 @@ import reactNative from "../assets/img/react-native.png"
 const Skills = () => {
 
     return (
-        <div>
-            <section className='skill' id='skills'>
-                <Container>
-                    <Row>
-                        <Col>
-                            <div className='skill-bx' >
-                                <h2>
-                                    Herramientas
-                                </h2>
-                                <p>Dentro de los lenguajes, tecnologías y herramientas que tengo experiencia se encuentran JavaScript, React.JS, React Native, PostgreSQL, Node.JS, Github, Redux, HTML y CSS.</p>
-                                <div className='skills-img-container'>
-                                    <div className='skill-container'>
-                                        <img src={javaScript} alt="javascript" />
-                                        <p>JavaScript</p>
-                                    </div>
-                                    <div className='skill-container'>
-                                        <img src={reactNative} alt="reactNative" />
-                                        <p>React.Js</p>
-                                    </div>
-                                    <div className='skill-container'>
-                                        <img src={NodeLogo} alt="Node.js" />
-                                        <p>Node.Js</p>
-                                    </div>
-                                    <div className='skill-container'>
-                                        <img src={postgresql} alt="PostgreSQL"/>
-                                        <p>PostgreSQL</p>
-                                    </div>
-                                    <div className='skill-container'>
-                                        <img src={express} alt="Express"/>
-                                        <p>Express.Js</p>
-                                    </div>
-                                    <div className='skill-container'>
-                                        <img src={bootstrap} alt="Bootstrap" />
-                                        <p>Bootstrap</p>
-                                    </div>
-                                    <div className='skill-container'>
-                                        <img src={git} alt="Git"/>
-                                        <p>Git</p>
-                                    </div>
-                                    <div className='skill-container'>
-                                        <img src={github} alt="Github"/>
-                                        <p>Github</p>
-                                    </div>
-                                    <div className='skill-container'>
-                                        <img src={html} alt="HTML"/>
-                                        <p>HTML</p>
-                                    </div>
-                                    <div className='skill-container'>
-                                        <img src={css} alt="CSS" />
-                                        <p>Css</p>
-                                    </div>
-                                    <div className='skill-container'>
-                                        <img src={redux} alt="Redux"/>
-                                        <p>Redux</p>
-                                    </div>
+        <section className='skill' id='skills'>
+            <Container>
+                <Row>
+                    <Col>
+                        <div className='skill-bx' >
+                            <h2>
+                                Herramientas
+                            </h2>
+                            <p>Dentro de los lenguajes, tecnologías y herramientas que tengo experiencia se encuentran JavaScript, React.JS, React Native, PostgreSQL, Node.JS, Github, Redux, HTML y CSS.</p>
+                            <div className='skills-img-container'>
+                                <div className='skill-container'>
+                                    <img src={javaScript} alt="javascript" />
+                                    <p>JavaScript</p>
+                                </div>
+                                <div className='skill-container'>
+                                    <img src={reactNative} alt="reactNative" />
+                                    <p>React.Js</p>
+                                </div>
+                                <div className='skill-container'>
+                                    <img src={NodeLogo} alt="Node.js" />
+                                    <p>Node.Js</p>
+                                </div>
+                                <div className='skill-container'>
+                                    <img src={postgresql} alt="PostgreSQL" />
+                                    <p>PostgreSQL</p>
+                                </div>
+                                <div className='skill-container'>
+                                    <img src={express} alt="Express" />
+                                    <p>Express.Js</p>
+                                </div>
+                                <div className='skill-container'>
+                                    <img src={bootstrap} alt="Bootstrap" />
+                                    <p>Bootstrap</p>
+                                </div>
+                                <div className='skill-container'>
+                                    <img src={git} alt="Git" />
+                                    <p>Git</p>
+                                </div>
+                                <div className='skill-container'>
+                                    <img src={github} alt="Github" />
+                                    <p>Github</p>
+                                </div>
+                                <div className='skill-container'>
+                                    <img src={html} alt="HTML" />
+                                    <p>HTML</p>
+                                </div>
+                                <div className='skill-container'>
+                                    <img src={css} alt="CSS" />
+                                    <p>Css</p>
+                                </div>
+                                <div className='skill-container'>
+                                    <img src={redux} alt="Redux" />
+                                    <p>Redux</p>
                                 </div>
                             </div>
-                        </Col>
-                    </Row>
-                </Container>
-            </section>
-        </div>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
     )
 }
 
