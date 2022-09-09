0804f1f2167440d17062daa61b1aec134d554c33
Maximiliano Minutillo <maximilminu@gmail.com>
2022-09-09 18:27:01 -0300

    modal

--- src/App.js
+++ src/App.js
@@ -6,14 +6,14 @@ import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
-import ModalForMobile from './components/ModalForMobile';
+// import ModalForMobile from './components/ModalForMobile';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Banner />
-      <ModalForMobile />
+      {/* <ModalForMobile /> */}
       <Skills />
       <Projects />
       <Contact />
