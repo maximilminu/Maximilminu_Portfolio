7df1452c1f645350c68c0c3d47f812a0f3f4f388
Maximiliano Minutillo <maximilminu@gmail.com>
2022-10-13 17:09:42 -0300

    app

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Footer from './components/Footer';
 // import ModalForMobile from './components/ModalForMobile';
 
 function App() {
+  
   return (
     <div className="App">
       <NavBar />
